0ee9f1024df5360ff2fb1418f53892006ef5b742
Hoyoung Jung <honux77@gmail.com>
2017-01-16 13:29:20 +0900

    Add C example

---
+++ week3/test/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+double PI = 3.1415926;
+
+int main(int argc, char *argv[]) {
+	int i = 5;
+	int j = 10;
+	printf("Hello, %d + %d = %d\n", i, j, i + j);
+	printf("C = %lf\n", 2 * 2 * PI);
+	return 0;
+}
